0551d69d4ff5bac6991889a7d4978244d328b8fc
RamGT <n1g9ab@gmail.com>
2022-02-18 14:37:07 +0300

    add CWRK

---
+++ CWRK/WRK/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+string PrintArray(string[] array)
+{
+    return $"[{String.Join(", ", array)}]";
+}
+
+int SearchString3(string[] array)
+{
+    int counter = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+string[] GenArray2(string[] array)
+{
+    string[] resultArray = new string[SearchString3(array)];
+    int j = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[j] = $"\"{item}\"";
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+string[] arr = new string[]{"a", "abdgf", ":-)", "sdafs", "Hook", "-2", "56566", "fox", "dog", "-0897865775655", "beer"};
+string[] resultArray = GenArray2(arr);
+
+Console.WriteLine($"{PrintArray(arr)} -> {PrintArray(resultArray)}\n");
